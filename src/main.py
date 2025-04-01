7b578b25cdd71df47452440079594bff25c173cd
KalanchoeChrysanthemum <157724777+KalanchoeChrysanthemum@users.noreply.github.com>
2025-04-01 03:02:58 -0400

    Further image processing, intro to AES encryption, and directory clean up

---
+++ src/main.py
@@ -0,0 +1,15 @@
+import cv2
+
+INPUT_IMG_PATH = '../img-in/nwero.jpg'
+OUTPUT_IMG_PATH = '../img-out/processed_nwero.jpg'
+
+def process_img(base_img):
+    return cv2.adaptiveThreshold(base_img, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 11, 2)
+
+def main():
+    img = cv2.imread(INPUT_IMG_PATH, cv2.IMREAD_GRAYSCALE)
+    cv2.imwrite("../img-out/gray_nwero.jpg", img)
+    cv2.imwrite(OUTPUT_IMG_PATH, process_img(img))
+
+if __name__ == '__main__':
+    main()
