11ec5c0a43831569f2591495eb49fdf05ca760e5
Cade1336 <124454831+Cade1336@users.noreply.github.com>
2025-04-21 08:44:38 -0400

    Adding real RFID code

--- arduino/rfid/rfid.ino
+++ arduino/rfid/rfid.ino
@@ -1,7 +1,21 @@
-void setup() {
+#include <SPI.h>
+#include <MFRC522.h>
 
+#define SS_PIN 10
+#define RST_PIN 7
+
+MFRC522 rfid(SS_PIN, RST_PIN);
+
+MFRC522::MIFARE_Key key;
+
+void setup() {
+Serial.begin(9600);
+SPI.begin();
+rfid.PCD_Init();
 }
 
 void loop() {
-  
+  // put your main code here, to run repeatedly:
+  if ( ! rfid.PICC_IsNewCardPresent())
+  return;
 }
