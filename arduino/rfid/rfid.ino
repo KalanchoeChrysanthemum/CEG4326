73d3d19e8d8fd32bb180ca682ed43120043214f6
KalanchoeChrysanthemum <157724777+KalanchoeChrysanthemum@users.noreply.github.com>
2025-04-21 12:46:30 -0400

    Update code

--- arduino/rfid/rfid.ino
+++ arduino/rfid/rfid.ino
@@ -1,7 +1,17 @@
-void setup() {
+#include <Arduino.h>
 
+#define OUTPUT_PIN 9
+#define FREQ 228000
+#define CHANNEL 0
+#define RESOLUTION 1
+#define DUTY_CYCLE 1
+
+void setup() {
+  ledcSetup(0, FREQ, RESOLUTION);
+  ledcAttachPin(OUTPUT_PIN, 0);
+  ledcWrite(0, DUTY_CYCLE);
 }
 
 void loop() {
-  
+
 }
--- rasp-pi/src/main.py
+++ rasp-pi/src/main.py
@@ -1,15 +1,46 @@
 import cv2
+import numpy as np
 
-INPUT_IMG_PATH = '../images/input/nwero.jpg'
-OUTPUT_IMG_PATH = '../images/output/processed_nwero.jpg'
+# Load the cropped 4x4 LED image
+image = cv2.imread('../images/input/a3.jpg', cv2.IMREAD_GRAYSCALE)
+cropped = image[500:1350, 600:1600]
+#cropped = cv2.GaussianBlur(cropped, (11, 11), 0)
+_, cropped = cv2.threshold(cropped, 220, 255, cv2.THRESH_BINARY)
 
-def process_img(base_img):
-    return cv2.adaptiveThreshold(base_img, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 11, 2)
+# Grid size
+rows, cols = 4, 4
 
-def main():
-    img = cv2.imread(INPUT_IMG_PATH, cv2.IMREAD_GRAYSCALE)
-    cv2.imwrite("../images/output/gray_nwero.jpg", img)
-    cv2.imwrite(OUTPUT_IMG_PATH, process_img(img))
+# Image dimensions
+height, width = cropped.shape[:2]
 
-if __name__ == '__main__':
-    main()
+# Size of each cell
+cell_h = height // rows
+cell_w = width // cols
+
+# Draw rectangles over each cell
+
+binary = ""
+
+for i in range(rows):
+    for j in range(cols):
+        x = j * cell_w
+        y = i * cell_h
+        roi = cropped[y:y+cell_h, x:x+cell_w]
+
+        avg_brightness = int(np.mean(roi))
+        status = '1' if avg_brightness > 20 else '0'
+        binary += status
+        
+        color = (0, 255, 0) if avg_brightness > 150 else (0, 0, 255)
+        cv2.rectangle(cropped, (x, y), (x + cell_w, y + cell_h), (255, 255, 255), 2)
+        text = str(avg_brightness)
+        text_size = cv2.getTextSize(text, cv2.FONT_HERSHEY_SIMPLEX, 0.5, 1)[0]
+        text_x = x + (cell_w - text_size[0]) // 2
+        text_y = y + (cell_h + text_size[1]) // 2
+        cv2.putText(cropped, text, (text_x, text_y), cv2.FONT_HERSHEY_SIMPLEX, 0.5, color, 1)
+    binary += '\n'
+
+# Save the output
+cv2.imwrite('../images/output/processed_a3.jpg', cropped)
+print(binary)
+
